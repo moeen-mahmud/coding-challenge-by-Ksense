cba2f6631a9de93595147caf0eab635d3975af95
Moeen Mahmud <write2fahim007@gmail.com>
2022-04-17 05:24:54 +0600

    showing the user posts

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 // Global constant
 const endpoint = "https://jsonplaceholder.typicode.com/users";
 const dataTable = document.getElementById("data-table");
+const modalContainer = document.getElementById("modal-container");
 
 // Fetch all the users from the api
 const fetchUser = async () => {
@@ -14,8 +15,21 @@ const fetchUser = async () => {
   }
 };
 
+// Show posts for a selected user
+const showFilteredPost = async (userId) => {
+  try {
+    const endpoint = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+    displayUserPosts(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
 const displayUsers = (users) => {
-  if (users.length !== 0) {
+  if (users?.length !== 0) {
     users?.forEach((user) => {
       const userId = user?.id;
       const userFullName = user?.name;
@@ -28,12 +42,37 @@ const displayUsers = (users) => {
           <td>${userFullName}</td>
           <td>${userEmail}</td>
           <td>
-            <button type="button" class="btn btn-primary">See Posts</button>
+            <button onclick="showFilteredPost('${userId}')" 
+              type="button"
+              class="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#showPostsModal"
+            >
+              See Posts
+            </button>
           </td>
         </tr>
       `;
 
       dataTable.appendChild(tableBody);
+    });
+  }
+};
+
+const displayUserPosts = (posts) => {
+  if (posts?.length !== 0) {
+    posts?.forEach((post) => {
+      const postTitle = post?.title;
+      const postBody = post?.body;
+
+      const postsDivisions = document.createElement("div");
+      postsDivisions.innerHTML = `
+        <div class="p-3 rounded border mb-3" >
+          <h4>${postTitle}</h4>
+          <p class="mb-2 text-secondary">${postBody}</p>
+        </div>
+      `;
+      modalContainer.appendChild(postsDivisions);
     });
   }
 };
